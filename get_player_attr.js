c5a222d40e47d5b4dbcea11cdc7a480c56a6e41b
Emily H <157163774+emheyboer@users.noreply.github.com>
2025-11-03 10:18:33 -0500

    comments

--- get_player_attr.js
+++ get_player_attr.js
@@ -1,3 +1,10 @@
+// the endpoint /player/timeline/poll returns an xml object which contains the following attributes:
+
+// state (playing/paused), duration (ms), time (ms), playQueueItemID, key,
+// ratingKey, playQueueID, playQueueVersion, containerKey, type,
+// itemType, volume (0-100), shuffle (0-1), repeat (0-2), controllable, machineIdentifier,
+// protocol, address, port
+
 const parser = new DOMParser();
 const xml = parser.parseFromString(http_data, 'application/xml');
 var value = xml.querySelector('Timeline[itemType="music"]').attributes[attr].value;
